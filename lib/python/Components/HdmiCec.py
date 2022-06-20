8491a89a13fb5ae68be4c52351e21d7891d39da7
builder08 <75539906+builder08@users.noreply.github.com>
2022-06-21 01:45:35 +0300

    Update HdmiCec.py

--- lib/python/Components/HdmiCec.py
+++ lib/python/Components/HdmiCec.py
@@ -1,5 +1,5 @@
 import struct, os, time
-from config import config, ConfigSelection, ConfigYesNo, ConfigSubsection, ConfigText, ConfigCECAddress, ConfigLocations, ConfigDirectory
+from Components.config import config, ConfigSelection, ConfigYesNo, ConfigSubsection, ConfigText, ConfigCECAddress, ConfigLocations, ConfigDirectory
 from enigma import eHdmiCEC, eActionMap
 from Tools.StbHardware import getFPWasTimerWakeup
 import NavigationInstance
